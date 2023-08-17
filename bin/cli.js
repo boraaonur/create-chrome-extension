a39df0789b20ae83a695cc2564cb9876c7dee481
Bora B <borabarteks@outlook.com>
2023-08-17 16:50:34 +0300

    feat(cli): prompt user for project name and clone into specified directory

--- bin/cli.js
+++ bin/cli.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 
 const { execSync } = require("child_process");
+const readline = require("readline");
+const fs = require("fs");
+const path = require("path");
 
 const runCommand = (command) => {
   try {
@@ -12,22 +15,43 @@ const runCommand = (command) => {
   return true;
 };
 
-const repoName = "create-chrome-extension";
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-console.log("Cloning repository...");
-const checkedOut = runCommand(
-  `git clone --depth 1 https://github.com/boraaonur/${repoName}`
-);
+rl.question("What project name do you want to use? ", (answer) => {
+  const userProjectDir = answer.trim();
+  const repoName = "create-chrome-extension";
 
-if (!checkedOut) process.exit(-1);
+  const createDirCommand = `mkdir ${userProjectDir}`;
+  const gitCheckoutCommand = `cd ${userProjectDir} && git clone --depth 1 https://github.com/boraaonur/create-chrome-extension`;
 
-console.log("Installing dependencies...");
-const installedDeps = runCommand(`cd ${repoName} && npm install`);
-if (!installedDeps) process.exit(-1);
+  runCommand(createDirCommand);
+  runCommand(gitCheckoutCommand);
 
-console.log("Cleaning...");
-runCommand(`rm -rf ${repoName}/bin`);
-runCommand(`rm ${repoName}/README.md`);
+  // Move the contents of the cloned directory to the user's specified directory
+  const sourcePath = path.join(userProjectDir, repoName);
+  const destPath = userProjectDir;
+  fs.readdirSync(sourcePath).forEach((file) => {
+    fs.renameSync(path.join(sourcePath, file), path.join(destPath, file));
+  });
 
-console.log("Chrome Extension Starter project is created.");
-console.log(`cd ${repoName} && npm start`);
+  // Remove the empty 'create-chrome-extension' directory
+  fs.rmdirSync(sourcePath);
+
+  // Installing dependencies
+  console.log("Installing dependencies...");
+  const installedDeps = runCommand(`cd ${userProjectDir} && npm install`);
+  if (!installedDeps) process.exit(-1);
+
+  // Clean unnecessary files and folders
+  console.log("Cleaning...");
+  runCommand(`rm -rf ${userProjectDir}/bin`);
+  runCommand(`rm ${userProjectDir}/README.md`);
+
+  console.log("Chrome Extension Starter project is created.");
+  console.log(`cd ${userProjectDir} && npm start`);
+
+  rl.close();
+});
