9ea909428a88912609aaea3d6e3b0000602d0e66
Bora B <borabarteks@outlook.com>
2023-08-17 16:39:57 +0300

    feat(cli): add cleanup steps to remove bin and README after cloning

--- bin/cli.js
+++ bin/cli.js
@@ -3,7 +3,6 @@
 const { execSync } = require("child_process");
 
 const runCommand = (command) => {
-  //
   try {
     execSync(`${command}`, { stdio: "inherit" });
   } catch (e) {
@@ -14,18 +13,21 @@ const runCommand = (command) => {
 };
 
 const repoName = "create-chrome-extension";
-const gitCheckoutCommand = `git clone --depth 1 https://github.com/boraaonur/create-chrome-extension`;
 
-const installDepsCommand = `cd ${repoName} && npm install`;
-
-console.log("Creating...");
-const checkedOut = runCommand(gitCheckoutCommand);
+console.log("Cloning repository...");
+const checkedOut = runCommand(
+  `git clone --depth 1 https://github.com/boraaonur/${repoName}`
+);
 
 if (!checkedOut) process.exit(-1);
 
 console.log("Installing dependencies...");
-const installedDeps = runCommand(installDepsCommand);
+const installedDeps = runCommand(`cd ${repoName} && npm install`);
 if (!installedDeps) process.exit(-1);
 
-console.log("NodeJS with Typescript project is created.");
+console.log("Cleaning...");
+runCommand(`rm -rf ${repoName}/bin`);
+runCommand(`rm ${repoName}/README.md`);
+
+console.log("Chrome Extension Starter project is created.");
 console.log(`cd ${repoName} && npm start`);
